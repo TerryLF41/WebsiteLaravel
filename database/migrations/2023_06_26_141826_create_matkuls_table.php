a05a032fdcc7d5dfdcc1100b405fb42284eb8c53
Enriek Naldo Patera <pateraenriek@gmail.com>
2023-06-26 22:06:33 +0700

    migrate matkul

--- database/migrations/2023_06_26_141826_create_matkuls_table.php
+++ database/migrations/2023_06_26_141826_create_matkuls_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('matkuls', function (Blueprint $table) {
             $table->id();
+            $table->string("nama_matkul");
+            $table->integer("sks");
+            $table->boolean("status");
             $table->timestamps();
         });
     }
