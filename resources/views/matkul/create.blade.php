a9bcbee094b1ac77c6247ed6a41dfea7c7a88cdb
TerryLF <if-21015@students.ithb.ac.id>
2023-06-27 17:12:26 +0700

    Update create.blade.php

--- resources/views/matkul/create.blade.php
+++ resources/views/matkul/create.blade.php
@@ -36,14 +36,14 @@
                                         @csrf
                                         <div class="mt-4">
                                             <x-input-label for="nama_matkul	" :value="__('Nama Matkul')" />
-                                            <x-text-input id="nama_matkul" name="nama_matkul" type="number" min="1"
-                                                max="4" class="mt-1 block w-80" autofocus autocomplete="nama_matkul	" />
+                                            <x-text-input id="nama_matkul" name="nama_matkul" type="text"
+                                               class="mt-1 block w-80" autofocus autocomplete="nama_matkul" />
                                             <x-input-error class="mt-2" :messages="$errors->get('nama_matkul')" />
                                         </div>
                                         <div class="mt-4">
                                             <x-input-label for="sks" :value="__('SKS')" />
-                                            <x-text-input id="sks" name="sks" type="text"
-                                                class="mt-1 block w-80" autofocus autocomplete="sks" />
+                                            <x-text-input id="sks" name="sks" type="number"  min="1"
+                                                max="4"  class="mt-1 block w-80" autofocus autocomplete="sks" />
                                             <x-input-error class="mt-2" :messages="$errors->get('sks')" />
                                         </div>
                                         <div class="mt-4">
