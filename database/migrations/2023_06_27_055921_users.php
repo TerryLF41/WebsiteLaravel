6a89348d1c516c33b328ff5268391738a26db03f
TerryLF <if-21015@students.ithb.ac.id>
2023-06-27 13:38:43 +0700

    Update 2023_06_27_055921_users.php

--- database/migrations/2023_06_27_055921_users.php
+++ database/migrations/2023_06_27_055921_users.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id')->primary();
+            $table->id('id');
             $table->string('username');
             $table->string('password');
             $table->timestamps('created_at');
