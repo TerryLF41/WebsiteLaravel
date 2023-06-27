4f805587eec5eb21f03f31f17b350a78b2ac7301
TerryLF <if-21015@students.ithb.ac.id>
2023-06-27 16:52:09 +0700

    matkulTime

---
+++ resources/views/matkulTime/create.blade.php
@@ -0,0 +1,62 @@
+<x-app-layout>
+    <div class="container-fluid">
+        <div class="content-header">
+            <div class="container-fluid">
+                @csrf
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Create Role') }}
+                </h2>
+            </div>
+        </div>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="cart-title">Buat Role</h3>
+                            </div>
+                            <div class="card-tools">
+                                @if (count($errors) > 0)
+                                    <div class="alert alert-danger">
+                                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+                                <div class="card-body">
+                                    <div>
+                                        <a class="btn btn-success" href="{{ route('matkuls.index') }}">
+                                            Back</a>
+                                    </div>
+                                    <form method="post" action="{{ route('roles.store') }}">
+                                        @csrf
+                                        <div class="mt-4">
+                                            <x-input-label for="hari" :value="__('Hari')" />
+                                            <x-text-input id="hari" name="hari" type="text"
+                                                class="mt-1 block w-80" autofocus autocomplete="hari" />
+                                            <x-input-error class="mt-2" :messages="$errors->get('hari')" />
+                                        </div>
+                                        <div class="mt-4">
+                                            <x-input-label for="jam" :value="__('Jam')" />
+                                            <x-text-input id="jam" name="name" type="time"
+                                                class="mt-1 block w-80" autofocus autocomplete="jam" />
+                                            <x-input-error class="mt-2" :messages="$errors->get('jam')" />
+                                        </div>
+
+                                        <div>
+                                            <x-primary-button class="mt-4">{{ __('Create') }}</x-primary-button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
